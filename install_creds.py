6a4b8800ed24dcbe315df577004c59c33cb1a033
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-03-04 13:47:59 -0500

    fix credential env name

--- install_creds.py
+++ install_creds.py
@@ -1,6 +1,6 @@
 import os
 
-google_credential = os.environ['CREDENTIAL']
+google_credential = os.environ['GS_CRDENTIALS']
 
 with open('~/.gdrive_private', 'w') as f:
     f.write(google_credential)
