86f6633c834876a451f1b2180906e1b34d310bd0
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-03-17 17:02:59 -0400

    moving google client key to env secret

--- install_creds.py
+++
@@ -1,7 +0,0 @@
-import os
-
-google_credential = os.getenv('GS_CREDENTIALS')
-home_path = os.getenv('HOME')
-
-with open(os.path.join(home_path,'.gdrive_private'), 'w') as f:
-    f.write(google_credential)
