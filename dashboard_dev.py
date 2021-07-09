978e73d251ae01532f038b018ba79bc6cd479dcb
Yulia Zamriy <yulia.zamriy@gmail.com>
2021-07-09 10:00:01 -0400

    change how env is written to file

--- dashboard_dev.py
+++ dashboard_dev.py
@@ -1,5 +1,14 @@
+import os
 import gspread
 import pandas as pd
+
+google_credential = os.getenv('GS_CREDENTIALS')
+home_path = os.getenv('HOME')
+print("gs creds: ", google_credential)
+print("home path: ", home_path)
+
+with open(os.path.join(home_path,'/.config/gspread/service_account.json'), 'w') as f:
+    f.write(google_credential)
 
 gc = gspread.service_account()
 sh = gc.open("ODD_DEV_Source_File")
